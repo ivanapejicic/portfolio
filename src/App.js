44b2b8af39aded322c40a2e52633b260671f3602
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-01 15:07:55 -0500

    Home page, mixins, styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './assets/pages/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <p>Ivana Pejicic PORTFOLIO</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path= '' element={<Home />} />
+            {/* <Route path= '/contact' element={<Contact /> } /> */}
+          {/* <Route path = '/points' element={<Points /> } /> */}
+        </Routes>      
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/assets/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import './Home.scss';
+
+function Home() {
+
+    return (
+        <div className='home'>
+
+        </div>
+    )
+}
+
+export default Home;

fd89d0e24d38f2bf07f5f92354e9e0f25b9e1236
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-02 20:26:03 -0500

    Nav bar added to the home page

--- src/assets/pages/Home/Home.js
+++ src/assets/pages/Home/Home.js
@@ -1,12 +1,42 @@
+// Home.js
+
+import React, { useState } from 'react';
 import './Home.scss';
 
 function Home() {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
 
     return (
         <div className='home'>
-
+            <div className='home__top'>
+                <button id='intro'>Home</button>
+                <div
+                    className='menu-icon'
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    {isHovered && (
+                        <div className='dropdown-menu'>
+                            <a href='#'>About</a>
+                            <a href='#'>Work</a>
+                            <a href='#'>Contact</a>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Home;

96113d2234e4cb87511e68049c11acffb3a0692d
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-05 11:44:22 -0500

    Home page made responsive. Fonts changed to Afta Sans Thin

--- src/assets/pages/Home/Home.js
+++ src/assets/pages/Home/Home.js
@@ -1,19 +1,25 @@
 import './Home.scss';
-import { Link
- } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function Home() {
 
     return (
-        <div className='home'>
-            <div className='home__top'>
-                <Link className='link' to='#' className='home__top-left'>Home</Link>
-                <nav className='home__top-right'>
-                    <Link className='link' to='#'>About</Link>
-                    <Link className='link' to='#'>Work</Link>
-                    <Link className='link' to='#'>Contact</Link>
-                </nav>
+        <>
+            <div className='home'>
+                <div className='home__top'>
+                    <Link className='link' to='#'>Home</Link>
+                    <nav className='home__top-right'>
+                        <Link className='link' to='#'>About</Link>
+                        <Link className='link' to='#'>Work</Link>
+                        <Link className='link' to='#'>Contact</Link>
+                    </nav>
+                </div>
             </div>
-        </div>
+            <div className='main'>
+                <h1 className='main__name'>Ivana Pejicic</h1>
+                <h2 className='main__title'>Full-stack Developer</h2>
+                <h3 className='main__description'>Crafting high-performance, elegant web applications.</h3>
+            </div>
+        </>
     );
 }
 

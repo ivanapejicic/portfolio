c2cf4e116527e82b3255f0f161eff8b3461c32fc
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-05 15:16:29 -0500

    Home page completed with styling

--- src/assets/pages/Home/Home.js
+++ src/assets/pages/Home/Home.js
@@ -9,10 +9,10 @@ function Home() {
 
     return (
         <>
-            <div className='home'>
-                <div className='home__top'>
+            <div className='header'>
+                <div className='header__top'>
                     <Link className='link' to='#'>Home</Link>
-                    <nav className='home__top-right'>
+                    <nav className='header__top-right'>
                         <Link className='link' to='#'>About</Link>
                         <Link className='link' to='#'>Work</Link>
                         <Link className='link' to='#'>Contact</Link>
@@ -23,7 +23,9 @@ function Home() {
                 <h1 className='main__name'>Ivana Pejicic</h1>
                 <h2 className='main__title'>Full-stack Developer</h2>
                 <h3 className='main__description'>Crafting high-performance, elegant web applications.</h3>
-                <img className='main__arrow' src={arrow_right} alt='arrow that takes to the next page'></img>
+                {/* <Link to="#">
+                    <img className='main__arrow' src={arrow_right} alt='arrow that takes to the next page'></img>
+                </Link> */}
             </div>
             <div className='footer'>
                 <Link to='https://www.linkedin.com/in/ivanapejicic/'>

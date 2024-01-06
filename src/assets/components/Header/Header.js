243470566bbcb939d3de0fd934c3ef77fd4db710
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-06 16:17:57 -0500

    Add Header component

---
+++ src/assets/components/Header/Header.js
@@ -0,0 +1,18 @@
+import './Header.scss';
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className='header__top'>
+                <Link className='link' to='#'>Home</Link>
+                <nav className='header__top-right'>
+                    <Link className='link' to='/about'>About</Link>
+                    <Link className='link' to='#'>Work</Link>
+                    <Link className='link' to='#'>Contact</Link>
+                </nav>
+            </div>
+        </div>
+    )
+}
+export default Header;
--- src/assets/pages/Home/Home.js
+++ src/assets/pages/Home/Home.js
@@ -3,22 +3,14 @@ import { Link } from 'react-router-dom';
 import linkedin from '../../images/linkedin.png';
 import github from '../../images/github.png';
 import email from '../../images/email.png';
+import Header from '../../components/Header/Header';
 import arrow_right from '../../images/arrow-right.png';
 
 function Home() {
 
     return (
         <div className='overlay'>
-            <div className='header'>
-                <div className='header__top'>
-                    <Link className='link' to='#'>Home</Link>
-                    <nav className='header__top-right'>
-                        <Link className='link' to='/about'>About</Link>
-                        <Link className='link' to='#'>Work</Link>
-                        <Link className='link' to='#'>Contact</Link>
-                    </nav>
-                </div>
-            </div>
+            <Header />
             <div className='main'>
                 <h1 className='main__name'>Ivana Pejicic</h1>
                 <h3 className='main__title'>Full-stack Developer</h3>

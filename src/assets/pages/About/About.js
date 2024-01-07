afea8784ae826f9dc6d9a42ecf7fc949da137dfb
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-07 15:06:51 -0500

    About section added

--- src/assets/pages/About/About.js
+++ src/assets/pages/About/About.js
@@ -12,8 +12,11 @@ function About() {
                     <h1 className='about__main-top__title'>Hey there</h1>
                     <img className = 'about__main-top__image' src={headshot} alt="Ivana's headshot"></img>
                 </div>
-                <p className='about__main-text'>My name is Ivana Pejicic. I was born and raised in Bosnia and Herzegovina. </p>
-                <p className='about__main-text'>My journey into the world of computer science and programming began right after high school.</p>
+                <p className='about__main-text'>My name is Ivana Pejicic. I was born and raised in Bosnia and Herzegovina.
+                My journey into the world of computer science and programming began right after high school. </p>
+                <p className='about__main-text'>I am a recent graduate of BrainStation's Software Engineering bootcamp, currently pursuing 
+                an Associate in Science degree at Miami Dade College with a GPA of 4.0. 
+                I hold a Bachelor of Science degree with a major in Mathematics and Computer Science from Bosnia. </p>
             </div>
             <Footer />
         </div>

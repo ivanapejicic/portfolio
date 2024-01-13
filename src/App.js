554486a4a0264b28582391adce2b70caa317c841
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-13 16:49:11 -0500

    Added Projects folder to images that contains screenshot of different projects that I worked on

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
           <Route path= '/about' element={<About /> } /> 
           <Route path = '/work' element={<Work /> } /> 
           <Route path = '/contact' element={<Contact /> } /> 
-
         </Routes>      
       </BrowserRouter>
     </div>

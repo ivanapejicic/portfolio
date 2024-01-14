df3f69098e95e4b8420ed3950be88d031c5f28a4
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-14 15:26:09 -0500

    Work page top

--- src/assets/pages/Work/Work.js
+++ src/assets/pages/Work/Work.js
@@ -1,9 +1,17 @@
 import './Work.scss';
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import checklist from '../../images/checklist.png';
 
 function Work() {
     return (
-        <>
-        </>
+        <div className='work'>
+            <Header />
+            <div className='work__top'>
+                <h1 className='work__top-title'>What I've done</h1>
+                <img className='work__top-img' src={checklist} alt='yellow checklist as an indicator that next section will show my work'></img>
+            </div>
+        </div>
     )
 }
 

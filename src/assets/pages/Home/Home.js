bb49d850636dce7d5f94fe50b49f593653b7099e
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2024-01-28 23:30:46 -0500

    Added 3 functions/timers to Home page to have rows/content show line by line

--- src/assets/pages/Home/Home.js
+++ src/assets/pages/Home/Home.js
@@ -1,16 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import './Home.scss';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 
 function Home() {
+    const [showName, setShowName] = useState(false);
+    const [showTitle, setShowTitle] = useState(false);
+    const [showDescription, setShowDescription] = useState(false);
+
+    useEffect(() => {
+        const nameTimer = setTimeout(() => {
+            setShowName(true);
+        }, 1000); 
+
+        const titleTimer = setTimeout(() => {
+            setShowTitle(true);
+        }, 2000); 
+
+        const descriptionTimer = setTimeout(() => {
+            setShowDescription(true);
+        }, 3000); 
+
+        return () => {
+            clearTimeout(nameTimer);
+            clearTimeout(titleTimer);
+            clearTimeout(descriptionTimer);
+        };
+    }, []);
 
     return (
         <div className='overlay'>
             <Header />
             <div className='main'>
-                <h1 className='main__name'>Ivana Pejicic</h1>
-                <h3 className='main__title'>Full-stack Developer</h3>
-                <h4 className='main__description'>Crafting high-performance, elegant web applications.</h4>
+                {showName && <h1 className='main__name'>Ivana Pejicic</h1>}
+                {showTitle && <h3 className='main__title'>Full-stack Developer</h3>}
+                {showDescription && <h4 className='main__description'>Crafting high-performance, elegant web applications.</h4>}
             </div>
             <Footer />
         </div>
